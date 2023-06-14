808edc6851ace619f047322e0fef589c03260994
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2023-06-13 20:37:05 -0500

    Initial commit

---
+++ src/main/java/com/example/laboratorio8/Laboratorio8Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio8Application.class, args);
+    }
+
+}
